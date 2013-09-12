5b29a74eb85acf525164febd9358e3c6aca0f7bb
richanagarkar <richanagarkar@Richas-MacBook-Pro.local>
2013-09-12 18:51:40 -0400

    first problem
    Leap Year

---
+++ 1.rkt
@@ -0,0 +1,5 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname |1|) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "htdp")))))
+;Number of seconds in leap year:
+(* 3600 24 366)
